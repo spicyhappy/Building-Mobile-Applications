9919b85a5b3a7a370fffe0038cae27752d8134f9
Xin Xin <mail@xin-squared.com>
2013-07-08 00:26:13 -0400

    Loading messages

--- Project 1 - Happy Day/main.js
+++ Project 1 - Happy Day/main.js
@@ -1,5 +1,6 @@
 function clearPage() {
 	// Wipes away previous data for a fresh start
+
 	$("#weatherCurrent .message").empty();
 	$("#weatherCurrent").empty();
 	$("#weatherFuture li").empty();
@@ -7,33 +8,27 @@ function clearPage() {
 }
 
 function colorize(temperature, opacity) {
+	// Returns color value for a given temperature
+
+	temperature = Math.max(0, Math.min(temperature, 100));
 	var r = Math.round((255*temperature)/100);
 	var g = Math.round((150*(100-temperature))/100); 
 	var b = Math.round((255*(100-temperature))/100);
 	var color = "rgba("+r+","+g+","+b+","+opacity+")";
 	return color;
-
 }
 
-function message(status) {
-	// Handles error and status messages
-	switch(status) {
-		case 'geolocation':
-			console.log('try another address');
-			break;
-		case 'blank input':
-			console.log('please type something in');
-			break;
-
-		case 'no news':
-			console.log('cannot find any news');
-			break;
-		default:
-			console.log('oops, something went wrong!');
+function message(status, disappear) {
+	$('.alert').html(status);
+	$('.alert').removeClass('hide');
+	if (disappear) {
+		setTimeout(function() {
+        	$('.alert').addClass('hide');
+		}, 2000);
 	}
 }
 
-function geolocationDeny() {
+function geolocationDenied() {
 	// Displays a message so the user does not see a blank screen.
 
 	$('#weatherCurrent').html("Hello! Use Happy Day to look up your local news and weather.");
@@ -43,10 +38,8 @@ function getNews(location) {
 	// Use Google News to display most recent news results from city
 
 	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20%3D%20'http%3A%2F%2Fnews.google.com%2Fnews%3Fq%3D"+location+"%26output%3Drss%26num%3D20'&format=json&diagnostics=true";
-	// Todo: throw an error if timeout
 	$.getJSON(url, function(data){
 		if (data.query.results === null) {
-			message('no rss');
 			return;
 		}
 		else {
@@ -55,9 +48,6 @@ function getNews(location) {
 				var articleSection = '#article'+(i+1);
 				if(typeof article.title !== 'undefined' && typeof article.link !== 'undefined') {
 					$(articleSection).append('<a href="'+article.link+'"><h1>'+article.title+'</h1></a>');
-				}
-				else {
-					message('no news articles');
 				}
 			}
 		}
@@ -69,30 +59,37 @@ function getWeather(location,latitude,longitude) {
 
 	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D'https%3A%2F%2Fapi.forecast.io%2Fforecast%2Fe053334212f4dd449660bd61b61b29e2%2F"+latitude+"%2C"+longitude+"'&format=json&diagnostics=true";
 	$.getJSON(url, function(data){
-		// Todo: detect api failure
-		var components = data.query.results.json.daily.data;
-		var todayTempMax = Math.round(components[0].temperatureMax);
-		var todayTempMin = Math.round(components[0].temperatureMin);
-		var tempAvg = (todayTempMax+todayTempMin)/2;
-
-		$('#weatherCurrent').html('<div class="weatherToday"><div>'+components[0].icon+'</div><div>'+todayTempMin+'-'+todayTempMax+'&deg;</div></div>Good day,<br>'+location.slice(0, location.indexOf(","))+'!');
 		
-		// Colorize background
-		$("body").css("background-color",colorize(tempAvg,1));
-		$('.weatherToday').css("background-color",colorize(tempAvg, .5));
+		if (data.query.results === null) {
+			return;
+		}
 
-		for (var i=1; i<8; i++) {
-			if (components[i].temperatureMax && components[i].temperatureMin && components[i].time && components[i].icon) {
+		else {
+			var components = data.query.results.json.daily.data;
+			var todayTempMax = Math.round(components[0].temperatureMax);
+			var todayTempMin = Math.round(components[0].temperatureMin);
+			var tempAvg = (todayTempMax+todayTempMin)/2;
 
-				var tempMax = Math.round(components[i].temperatureMax);
-				var tempMin = Math.round(components[i].temperatureMin);
-				var tempAvg = (tempMax+tempMin)/2;
-				var timeStamp = new Date(components[i].time*1000);
-				var day = timeStamp.getUTCDay();
-				var dayOfWeek = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+			$('#weatherCurrent').html('<div class="weatherToday"><div>'+components[0].icon+'</div><div>'+todayTempMin+'-'+todayTempMax+'&deg;</div></div>Good day,<br>'+location.slice(0, location.indexOf(","))+'!');
+			
+			// Colorize background
+			$("body").css("background-color",colorize(tempAvg,1));
+			$('.weatherToday').css("background-color",colorize(tempAvg, .5));
 
-				$('#weatherFuture .day'+[i]).html('<div>'+dayOfWeek[day]+'</div><div>'+components[i].icon+'</div><div>'+tempMin+'-'+tempMax+'&deg;');
-				$('#weatherFuture .day'+[i]).css("background-color",colorize(tempAvg,.5));
+			// Setup 7 days into the future
+			for (var i=1; i<8; i++) {
+				if (components[i].temperatureMax && components[i].temperatureMin && components[i].time && components[i].icon) {
+
+					var tempMax = Math.round(components[i].temperatureMax);
+					var tempMin = Math.round(components[i].temperatureMin);
+					var tempAvg = (tempMax+tempMin)/2;
+					var timeStamp = new Date(components[i].time*1000);
+					var day = timeStamp.getUTCDay();
+					var dayOfWeek = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+
+					$('#weatherFuture .day'+[i]).html('<div>'+dayOfWeek[day]+'</div><div>'+components[i].icon+'</div><div>'+tempMin+'-'+tempMax+'&deg;');
+					$('#weatherFuture .day'+[i]).css("background-color",colorize(tempAvg,.5));
+				}
 			}
 		}
 	});
@@ -103,7 +100,7 @@ function getLocation(url) {
 
 	$.getJSON(url, function(data){
 		if(data.status === 'ZERO_RESULTS') {
-			message('geolocation');
+			message("We couldn't find that city or zip. Please try again!");
 			return;
 		}
 		var components = data.results[0].address_components;
@@ -119,7 +116,7 @@ function getLocation(url) {
 		}
 
 		if (typeof city === 'undefined' || state === 'undefined') {
-			message('geolocation');
+			message("We couldn't find that city or zip. Please try again!", true);
 			return;
 		}
 
@@ -131,8 +128,8 @@ function getLocation(url) {
 }
 
 function loadSearch(location,latitude,longitude) {
-	// Set up the page for a new search 
-
+	// Set up the page for a new search
+	$('#locationSearch').val(location);
 	$('header .shortcuts').addClass('hide');
 	$('#locationSearch').blur();
 	clearPage();
@@ -165,6 +162,7 @@ function saveHistory(location) {
 
 function setPosition(position) {
 	// Takes position from geolocation and passes it to loadSearch
+
 	var latitude = position.coords.latitude;
 	var longitude = position.coords.longitude;
 	var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+position.coords.latitude+','+position.coords.longitude+'&sensor=true';
@@ -175,7 +173,8 @@ function setPosition(position) {
 $(document).ready(function() {
 
 	// Look up current location and pass to loadSearch. If denied, load welcome message.
-	navigator.geolocation.getCurrentPosition(setPosition, geolocationDeny);
+	message("Loading...");
+	navigator.geolocation.getCurrentPosition(setPosition, geolocationDenied);
 
 	// Load last 5 searches and search for the most recent search
 	if (typeof localStorage.pastSearches !== 'undefined') {
@@ -193,8 +192,7 @@ $(document).ready(function() {
 
 		if (navigator.geolocation) {
 			$('.shortcuts').prepend('<a id="getLocation">Get current location</a>');
-			// Todo: Throw an error message if geolocation times out
-			$('#getLocation').click(function(){navigator.geolocation.getCurrentPosition(setPosition);});
+			$('#getLocation').click(function(){message("Loading..."); navigator.geolocation.getCurrentPosition(setPosition);});
 		}
 
 		$('#historyList li').click(function(){
@@ -211,10 +209,11 @@ $(document).ready(function() {
 
 	// Take user value and turn into city + state, and pass to loadSearch
 	$('header form').on('submit', function(){
+		message("Loading...");
 		var searchBox = $('#locationSearch').val();
 
 		if (searchBox.length === 0) {
-			message('blank input');
+			message('Blank input.',true);
 			return false;
 		}
 
@@ -226,3 +225,7 @@ $(document).ready(function() {
 	});
 
 }); // End $(document).ready
+
+$(document).ajaxStop(function() {
+      $('.alert').addClass('hide');
+});
